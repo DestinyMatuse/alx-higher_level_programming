acd1c704670ab251a66e000546a300c45b5059f2
DestinyMatuse <lillymatuse@gmail.com>
2023-10-09 00:13:54 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,50 @@
-#include "lists.h"
-/**
- * palindrome - check if is palindrome with recursion
- * @l: l
- * @r: r
- *
- * Return: 1 palindrome, 0 not palindrome
- */
-int palindrome(listint_t **l, listint_t *r)
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_bytes(PyObject *p)
 {
-	int response;
+	long int size;
+	int i;
+	char *trying_str = NULL;
 
-	if (r != NULL)
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
 	{
-		response = palindrome(l, r->next);
-		if (response != 0)
-		{
-			response = (r->n == (*l)->n);
-			*l = (*l)->next;
-			return (response);
-		}
-		return (0);
-
+		printf("[ERROR] Invalid Bytes Object\n");
+		return;
 	}
-	return (1);
+
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
+
+	printf("size: %li\n", size);
+	printf("trying string: %s\n", trying_str);
+	if (size < 10)
+		printf("first %li bytes:", size + 1);
+	else
+		printf("first 10 bytes:");
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", trying_str[i]);
+	printf("\n");
 }
 
-/**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: head of linked list
- *
- * Return: 1 palindrome, 0 not palindrome
- */
-int is_palindrome(listint_t **head)
+void print_python_list(PyObject *p)
 {
-	if (head == NULL)
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", list->allocated);
+	for (i = 0; i < size; i++)
 	{
-		return (0);
+		type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(list->ob_item[i]);
 	}
-	return (palindrome(head, *head));
 }
+
