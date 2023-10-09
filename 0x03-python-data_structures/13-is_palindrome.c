48c467345c5668009f9528755c7313be0011752d
DestinyMatuse <lillymatuse@gmail.com>
2023-10-09 00:30:43 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,66 +1,34 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
-#include <bytesobject.h>
+#include "lists.h"
 
 /**
- * print_python_bytes - Prints information about a Python bytes object.
- * @p: Pointer to the Python object to be analyzed.
+ * is_palindrome - checks if a linked list is a palindrome
  *
- * This function prints details about a Python bytes object, including its size
- * and the first 10 bytes of its content in hexadecimal representation. If the
- * object is not a valid bytes object, it prints an error message.
- */
-void print_python_bytes(PyObject *p)
-{
-	long int size;
-	int i;
-	char *trying_str = NULL;
-
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("[ERROR] Invalid Bytes Object\n");
-		return;
-	}
-
-	PyBytes_AsStringAndSize(p, &trying_str, &size);
-
-	printf("size: %ld\n", size);
-	printf("trying string: %s\n", trying_str);
-	if (size < 10)
-		printf("first %ld bytes:", size + 1);
-	else
-		printf("first 10 bytes:");
-	for (i = 0; i <= size && i < 10; i++)
-		printf(" %02hhx", trying_str[i]);
-	printf("\n");
-}
-
-/**
- * print_python_list - Prints information about a Python list object.
- * @p: Pointer to the Python list object to be analyzed.
+ * @head: the head address of the linked list
  *
- * This function prints details about a Python list object, including its size
- * and the type of each element in the list. If an element is a bytes object,
- * it calls print_python_bytes to print additional information about the bytes object.
+ * Return: 1 if it's a palindrome, 0 else
  */
-void print_python_list(PyObject *p)
+
+int is_palindrome(listint_t **head)
 {
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *list = (PyListObject *)p;
-	const char *type;
+	listint_t *cur = *head;
+	int tab[2048], i = 0, j = 0;
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-	for (i = 0; i < size; i++)
+	if (*head)
 	{
-		type = (list->ob_item[i])->ob_type->tp_name;
-		printf("Element %d: %s\n", i, type);
-		if (!strcmp(type, "bytes"))
-			print_python_bytes(list->ob_item[i]);
-	}
-}
+		while (cur)
+		{
+			tab[i] = cur->n;
+			cur = cur->next;
+			i++;
+		}
 
+		while (j < i / 2)
+		{
+			if (tab[j] == tab[i - j - 1])
+				j++;
+			else
+				return (0);
+		}
+	}
+	return (1);
+}
