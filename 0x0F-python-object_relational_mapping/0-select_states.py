d44c6fd76844916296944cd813a77db3f5e729aa
DestinyMatuse <lillymatuse@gmail.com>
2024-01-12 18:06:06 +0200

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,46 +1,15 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        # Establish a database connection
-        db = MySQLdb.connect(
-            host='localhost',
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
-
-        # Create a cursor
-        cur = db.cursor()
-
-        # Execute the SQL query
-        cur.execute("SELECT * FROM states")
-
-        # Fetch all rows
-        table = cur.fetchall()
-
-        # Print the results
-        for row in table:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close cursor and database connection in a 'finally' block
-        if cur:
-            cur.close()
-        if db:
-            db.close()
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
