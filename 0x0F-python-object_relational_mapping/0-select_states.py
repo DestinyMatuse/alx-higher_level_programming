4380dcc18b2c2298a88ffb7a8d11773316165959
DestinyMatuse <lillymatuse@gmail.com>
2024-01-12 17:48:17 +0200

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+    try:
+        # Establish a database connection
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
         )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    table = cur.fetchall()
+        # Create a cursor
+        cur = db.cursor()
 
-    for row in table:
-        print(row)
+        # Execute the SQL query
+        cur.execute("SELECT * FROM states")
 
-    cur.close()
-    db.close()
+        # Fetch all rows
+        table = cur.fetchall()
+
+        # Print the results
+        for row in table:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close cursor and database connection in a 'finally' block
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
