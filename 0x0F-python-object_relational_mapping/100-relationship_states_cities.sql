0415cbbc316ea2cc0bea1a56d547e19952553156
DestinyMatuse <133487921+DestinyMatuse@users.noreply.github.com>
2024-01-13 20:28:12 +0200

    Update 100-relationship_states_cities.sql

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -1,5 +1,4 @@
--- Create the database hbtn_0e_100_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
+CREATE DATABASE if not EXIST hbtn_0e_100_usa;
 USE hbtn_0e_100_usa;
 SELECT * FROM states;
 SELECT * FROM cities;
