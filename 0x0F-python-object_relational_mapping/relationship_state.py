2fbe49e06a223bd36ba7b8dc90422aa8160a5b16
DestinyMatuse <133487921+DestinyMatuse@users.noreply.github.com>
2024-01-13 20:49:26 +0200

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -7,14 +7,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
-
+Base = declarative_base(metadata=mymetadata);
 
 class State(Base):
     """
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    id = Column(Integer, unique=True, nullable=False, primary_key=True);
+    name = Column(String(128), nullable=False);
+    cities = relationship("City", backref="states");
